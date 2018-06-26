0fbadb9c7a27afced2335b094b16a54e5eb45e04
triste <zhenglfsir@gmail.com>
2018-06-26 21:06:43 +0800

    增加prettier规则

---
+++ packages/eslint-config-fatc/eslint-config-prettier.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    browser: true,
+    jest: true
+  },
+  extends: ['prettier']
+}
